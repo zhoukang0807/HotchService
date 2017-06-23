3221b8a1961b6554fbe958ad69dbfe1800ad0a8e
Administrator <15251895180@163.com>
2017-06-23 18:03:54 +0800

    新增

--- config/config.js
+++ config/config.js
@@ -1,5 +1,17 @@
 module.exports = {
     port: 8089,
     mongodb: 'mongodb://127.0.0.1:27017/hotchPotch',
+    email: {
+        //支持邮箱
+        //"126" "163" "1und1" "AOL" "DebugMail" "DynectEmail" "FastMail" "GandiMail" "Gmail" "Godaddy"
+        // "GodaddyAsia" "GodaddyEurope" "hot.ee" "Hotmail" "iCloud" "mail.ee" "Mail.ru" "Maildev" "Mailgun"
+        // "Mailjet" "Mailosaur" "Mandrill" "Naver" "OpenMailBox" "Outlook365" "Postmark" "QQ" "QQex"
+        // "SendCloud" "SendGrid" "SendinBlue" "SendPulse" "SES" "SES-US-EAST-1" "SES-US-WEST-2"
+        // "SES-EU-WEST-1" "Sparkpost" "Yahoo" "Yandex" "Zoho" "qiye.aliyun"
+        service: "qq",
+        port: "465",
+        user: "MIGU_OP_SYS0001@qq.com",
+        pass: "psyfclahlfjbddgj"
+    },
 };
 
---
+++ util/utils.js
@@ -0,0 +1,28 @@
+var config = require('../config/config');
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: config.email.host, // 主机
+    secureConnection: true, // 使用 SSL
+    port: config.email.port, // SMTP 端口
+    auth: {
+        user: config.email.user,
+        pass: config.email.pass
+    }
+});
+exports.sendEmail = function (email, title, content, html) {
+    var mailOptions = {
+        from: config.email.user, // 发件地址
+        to: email, // 收件列表
+        subject: title, // 标题
+        text: content,
+        html: html
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        console.log(error);
+        if (error) {
+            return error;
+        } else {
+            return "Success";
+        }
+    });
+}
