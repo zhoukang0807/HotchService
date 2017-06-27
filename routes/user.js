4afb68116923fd6d85eae275fe7ac2e1641faff8
Administrator <15251895180@163.com>
2017-06-27 16:27:40 +0800

    新增

--- routes/user.js
+++ routes/user.js
@@ -11,13 +11,18 @@ router.get('/', function(req, res, next) {
 router.post('/register', function(req, res, next) {
     var postData = req.body;
     console.log(postData);
-    Utils.sendEmail("731364471@qq.com","注册验证码","","<h3>页面HTML</h3>")
     if(postData.userName=="admin" && postData.password=="admin"){
         res.send({resultCode:"0000",resultDesc:"注册成功！",user:postData});
         log.info("用户注册成功|用户名"+postData.userName+"|密码"+postData.password+"|邮箱"+postData.email);
     }else{
         res.send({resultCode:"0001",resultDesc:"注册失败！重复"});
     }
+});
+router.post('/send/indentify', function(req, res, next) {
+    var postData = req.body;
+    console.log(postData);
+    Utils.sendEmail(postData.email,"注册验证码","","<h3>页面HTML</h3>")
+    res.send({resultCode:"0000",resultDesc:"发送验证码成功！",code:"FTR!@#"});
 });
 router.post('/login', function(req, res, next) {
     var postData = req.body;
