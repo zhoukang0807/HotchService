9b2d2dde34b765446160b67119bdcb4a0a6525dc
zk <15251895180@163>
2017-07-15 19:09:55 +0800

    开发中

--- routes/contact.js
+++ routes/contact.js
@@ -7,29 +7,56 @@ var log4js = require('log4js');
 log4js.configure("./log4js.json");
 var log = log4js.getLogger('contact');
 router.get('/add', function (req, res, next) {
-    var friends=[
-        {
-            userId:1499083182572912,
-            userName:"kangz",
-            sign:"这是一个个性签名",
-            remark:"周康",
-            nickName:"霜龙？。。。"
-        },
-        {
-            userId:1499083182572913,
-            userName:"admin",
-            sign:"这是一个个性签名",
-            nickName:"管理员？。。。"
-        },
-    ];
-    User.update({userId:1499084091067978},{$set: {friends: friends}},function (err,row) {
-        if(err){
-            res.send(err);
-        }
-        else{
-            res.send(row);
+    log.info(req.query);
+    const form = req.query.form;
+    const receiver = req.query.receiver;
+
+    User.findOne({userName:form},function (err,data) {
+        if(data){
+            User.findOne({userName:receiver},function (err,row) {
+                if(row){
+                    let friends=row.friends;
+                    let param={
+                        userId:data.userId,
+                        userName:data.userName,
+                        sign:data.sign,
+                        remark:"",
+                        nickName:data.nickName
+                    };
+                    friends.push(param);
+                    User.update({userName:receiver},{$set: {friends: friends}},function (err,result1) {
+                        if(err){
+                            res.send({resultCode: constant.resultCode.Error_Code_DB, resultDesc:"数据库异常！"});
+                        }
+                        else{
+                            let friends2=data.friends;
+                            var param2={
+                                userId:row.userId,
+                                userName:row.userName,
+                                sign:row.sign,
+                                remark:"",
+                                nickName:row.nickName
+                            };
+                            friends2.push(param2);
+                            User.update({userName:form},{$set: {friends: friends2}},function (err,result2) {
+                                if(err){
+                                    res.send({resultCode: constant.resultCode.Error_Code_DB, resultDesc:"数据库异常！"});
+                                }
+                                else{
+                                    res.send({resultCode: constant.resultCode.Success_Code, resultDesc:"操作成功！",userName:receiver});
+                                }
+                            })
+                        }
+                    })
+                }else{
+                    res.send({resultCode: constant.resultCode.Error_Code_NoExit, resultDesc:"用户信息不存在！"});
+                }
+            })
+        }else{
+            res.send({resultCode: constant.resultCode.Error_Code_NoExit, resultDesc:"用户信息不存在！"});
         }
     })
+
 });
 
 
